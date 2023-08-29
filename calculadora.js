760eb890e49473318eed17d81846bc29ec60b114
arthur <arthur>
2023-08-29 15:31:38 -0300

    safdas

--- calculadora.js
+++ calculadora.js
@@ -1,5 +1,9 @@
 var somar = require("./modulos/somar");
 var media = require("./modulos/media");
+var multi = require("./modulos/somar");
+var dividi = require("./modulos/media");
 
 console.log(somar(20,90));
 console.log(media(20,90));
+console.log(multi(20,90));
+console.log(dividi(20,90));
